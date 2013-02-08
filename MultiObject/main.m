83f10aa51608fb5e3f667a60cd27e9460d7e0956
mettoboshi <mettoboshi.1108@gmail.com>
2013-02-08 20:18:06 +0900

    Initial Commit

---
+++ MultiObject/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MultiObject
+//
+//  Created by mettoboshi on 13/02/08.
+//  Copyright mettobo 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
